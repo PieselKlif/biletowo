884d998a8bca6bb1d7de554bd92d54e855ba7059
PieselKlif <pieselklif@gmail.com>
2025-03-31 20:26:08 +0200

    fix

--- views/footer.php
+++ views/footer.php
@@ -2,17 +2,17 @@
 	<div class="content">
 		<div class="logo">
 			<img src="/Logo.svg" alt="Logo">
-			<h2>BILETOWO</h2>
+			<h1>BILETOWO</h1>
 		</div>
 		<div>
-			<h3>Informacje</h3>
+			<h4>Informacje</h4>
 			<a href="/about">O nas</a>
 			<a href="/kontakt">Kontakt</a>
 			<a href="/polityka-prywatnosci">Polityka prywatności</a>
 			<a href="/regulamin">Regulamin</a>
 		</div>
 		<div>
-			<h3>Pomoc</h3>
+			<h4>Pomoc</h4>
 			<a href="/faq">FAQ - Najczęstsze pytania</a>
 			<a href="/zwroty-reklamacje">Zwroty i reklamacje</a>
 			<a href="/metody-platnosci">Metody płatności</a>
---
+++ views/home.php
@@ -0,0 +1,11 @@
+<main>
+	<section>
+		<h2>Polecane Wydarzenia</h2>
+	</section>
+	<section>
+		<h2>Polecane Artyści</h2>
+	</section>
+	<section>
+		<h2>Polecane Miejsca</h2>
+	</section>
+</main>
