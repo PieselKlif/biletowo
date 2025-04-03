b3fcd34db45d636c85f1d22b136c52bcbc0b0f2a
PieselKlif <pieselklif@gmail.com>
2025-04-03 18:31:47 +0200

    fix

--- views/artist.php
+++ views/artist.php
@@ -1,3 +1,4 @@
+<?php if (!defined("ABSPATH")) die("Brak dostępu"); ?>
 <?php function artist($slug, $img, $name) { ?>
 <a href="<?= WebConf::$artysci . $slug ?>" class="artist">
 	<div style="background-image: url('<?= WebConf::$uploadDir . $img ?>');"></div>
--- views/venues.php
+++ views/venues.php
@@ -1,3 +1,4 @@
+<?php if (!defined("ABSPATH")) die("Brak dostępu"); ?>
 <?php function venues($slug = "pge-narodowy", $img = "narodowy.jpeg", $name = "PGE Narodowy", $city = "Warszawa") { ?>
 <a href="<?= WebConf::$miejsca . $slug ?>" class="venues">
 	<div style="background-image: url('<?= WebConf::$uploadDir . $img ?>');"></div>
